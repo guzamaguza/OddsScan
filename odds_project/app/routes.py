faab5d21ff749fbdf122f9a5fbc5847629575840
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-03-28 00:16:35 -0400

    Delete odds_project/app directory

--- odds_project/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# Define the blueprint
-main = Blueprint('main', __name__)
-
-# Define your routes
-@main.route('/')
-def home():
-    return render_template("index.html")
