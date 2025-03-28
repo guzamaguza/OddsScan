4ab71b3c43144198931ddc1c8227cd8c50169d68
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-03-28 00:05:53 -0400

    Delete odds_project directory

--- odds_project/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    DATABASE_URI = 'sqlite:///odds.db'  # Change to PostgreSQL/MySQL if needed
-    DEBUG = True
