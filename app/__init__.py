e568d916baf614c9b084c7b48c84e8a131d82439
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 00:48:52 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+# Initialize SQLAlchemy
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+    
+    # Load the configuration from config.py
+    app.config.from_object(Config)
+    
+    # Initialize the database with the app
+    db.init_app(app)
+    
     # Import the main Blueprint after the app is created to avoid circular imports
     from app.routes import main  # Correct import based on where your Blueprint is defined
     app.register_blueprint(main)  # Register the Blueprint
 
     return app
+
 
