8655e1fdf6e549e0a84f8d778ae33de771d54fa6
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 09:45:26 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,7 @@
 import os
 
 class Config:
-    # Add your PostgreSQL URI here, either the internal or external connection
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://odds_db_p_user:OpPKdTCSCL6klAW12bpahexoQdiabrRH@dpg-cvn96b3uibrs73bb4910-a.oregon-postgres.render.com/odds_db_p'
-    
-    # Additional configuration options for SQLAlchemy
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disables Flask-SQLAlchemy modification tracking (optional but recommended)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # This should already be in use
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-
+    ODDS_API_KEY = os.getenv("ODDS_API_KEY")  # Your new variable
