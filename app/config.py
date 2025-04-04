d6ed3102ef3d59472dcc4e72b815dde6f9785c9a
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 10:17:54 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Use Render's default
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ODDS_API_KEY = os.environ.get('ODDS_API_KEY')
 
