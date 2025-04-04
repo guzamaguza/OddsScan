4d1bf2bbd4cc8ada8cb33c8506d4a78de0621243
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 11:59:54 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,3 +4,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', '')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ODDS_API_KEY = os.getenv('ODDS_API_KEY', '')
+
+print(f"[DEBUG] DATABASE_URL: {os.getenv('DATABASE_URL')}")
