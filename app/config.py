326a41dc23da125e4ffb0a9026b9cd34b7076597
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 01:04:04 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # Use Render's PostgreSQL URL
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Add your PostgreSQL URI here, either the internal or external connection
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'postgresql://odds_db_p_user:OpPKdTCSCL6klAW12bpahexoQdiabrRH@dpg-cvn96b3uibrs73bb4910-a.oregon-postgres.render.com/odds_db_p'
+    
+    # Additional configuration options for SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disables Flask-SQLAlchemy modification tracking (optional but recommended)
+
 
 
