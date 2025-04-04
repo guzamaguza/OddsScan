9bc37142ead190ad2cd990dbbb0dea8eb89d71d4
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 10:13:47 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # This should already be in use
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Use Render's default
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ODDS_API_KEY = os.environ.get('ODDS_API_KEY')
 
-    ODDS_API_KEY = os.getenv("ODDS_API_KEY")  # Your new variable
