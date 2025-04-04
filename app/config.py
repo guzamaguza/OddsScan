8d0ea77fa557bd7e287e069a03165de9c3971426
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-04 11:55:40 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', '')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ODDS_API_KEY = os.environ.get('ODDS_API_KEY')
-
+    ODDS_API_KEY = os.getenv('ODDS_API_KEY', '')
