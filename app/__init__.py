4226a514352f9f2cf7482c85ac6efa42e0bc615a
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 19:02:51 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-
+from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 
@@ -16,6 +17,8 @@ def create_app():
     app.register_blueprint(main)
 
     return app
+
+
 
 
 
