83d5a782a37b1d6a3fe8668a23b71bc1b5ca1139
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 18:43:47 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,19 +5,22 @@ from app import db
 # from app import db  <-- You don't need to declare it again here, it's already initialized in app/__init__.py
 
 class Event(db.Model):
-    id = db.Column(db.String(10), primary_key=True)  # Adjust length of string if necessary
-    name = db.Column(db.String(100), nullable=False)
+    __tablename__ = 'event'
+    
+    id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(100))
+    odds = db.relationship('Odds', backref='event', lazy=True)
 
     def __repr__(self):
-        return f"<Event {self.name}>"
+        return f'<Event {self.name}>'
 
 class Odds(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    event_id = db.Column(db.String(10), db.ForeignKey('event.id'), nullable=False)
-    time = db.Column(db.DateTime, default=datetime.utcnow)
-    odds_value = db.Column(db.Float, nullable=False)
-
-    event = db.relationship('Event', backref=db.backref('odds', lazy=True))
+    __tablename__ = 'odds'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.String(50), db.ForeignKey('event.id'), nullable=False)
+    time = db.Column(db.String(50))
+    odds_value = db.Column(db.Float)
 
     def __repr__(self):
-        return f"<Odds {self.odds_value} for Event {self.event_id}>"
+        return f'<Odds {self.odds_value}>'
