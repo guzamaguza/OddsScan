d3f7525b453492af03ca8870bdc24698b0be08ef
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 17:46:49 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 import os
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', '')
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL is not set in the environment variables.")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ODDS_API_KEY = os.getenv('ODDS_API_KEY', '')
-
-print(f"[DEBUG] DATABASE_URL: {os.getenv('DATABASE_URL')}")
