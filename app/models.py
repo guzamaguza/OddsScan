73083e1892ddab582ac2845fec9013386638c36a
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 16:57:18 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from app import db
 
-db = SQLAlchemy()
+# We do NOT create db here. It should be imported from app
+# from app import db  <-- You don't need to declare it again here, it's already initialized in app/__init__.py
 
 class Event(db.Model):
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.String(10), primary_key=True)  # Adjust length of string if necessary
     name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Event {self.name}>"
 
 class Odds(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -15,3 +18,6 @@ class Odds(db.Model):
     odds_value = db.Column(db.Float, nullable=False)
 
     event = db.relationship('Event', backref=db.backref('odds', lazy=True))
+
+    def __repr__(self):
+        return f"<Odds {self.odds_value} for Event {self.event_id}>"
