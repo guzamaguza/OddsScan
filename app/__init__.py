caf1023ac5f1d46a0f9da01b92ce2874595a5162
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 19:05:33 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,19 @@ from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()  # Initialize Flask-Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Apply the configuration
 
     db.init_app(app)  # Initialize SQLAlchemy with the app
+    migrate.init_app(app, db)  # Initialize Flask-Migrate with the app and db
 
     # Register Blueprints (or routes)
     from app.routes import main
     app.register_blueprint(main)
 
     return app
-
-
-
 
 
