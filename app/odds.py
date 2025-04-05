358fc63f711622eea3c6d01c148a6603e45d7551
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 16:38:05 -0400

    Update odds.py

--- app/odds.py
+++ app/odds.py
@@ -4,7 +4,7 @@ import certifi
 from flask import Flask, render_template, request, jsonify, current_app
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-from models import db, Odds, Event  # Make sure to import Event
+from app.models import db, Odds, Event  # Make sure to import Event
 
 # Flask App Initialization
 app = Flask(__name__)
