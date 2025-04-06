791456decd09ab267c26a4c48a66c80797cdc74e
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-05 20:50:32 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,30 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from app import db
-# We do NOT create db here. It should be imported from app
-# from app import db  <-- You don't need to declare it again here, it's already initialized in app/__init__.py
-
-class Event(db.Model):
-    __tablename__ = 'event'
-    
-    id = db.Column(db.String(50), primary_key=True)
-    name = db.Column(db.String(100))
-    odds = db.relationship('Odds', backref='event', lazy=True)
-
-    def __repr__(self):
-        return f'<Event {self.name}>'
+from . import db
 
 class Odds(db.Model):
-    __tablename__ = 'odds'
-    
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.String(50), db.ForeignKey('event.id'), nullable=False)
-    time = db.Column(db.String(50))
-    odds_value = db.Column(db.Float)
-
-    def __repr__(self):
-        return f'<Odds {self.odds_value}>'
-
+    event_id = db.Column(db.String)
+    home_team = db.Column(db.String)
+    away_team = db.Column(db.String)
+    start_time = db.Column(db.String)
+    bookmaker = db.Column(db.String)
+    market = db.Column(db.String)
+    outcome = db.Column(db.String)
+    price = db.Column(db.Float)
+    point = db.Column(db.Float)
+    timestamp = db.Column(db.String)
+    odds_type = db.Column(db.String)
 
 
 
