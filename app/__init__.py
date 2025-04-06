bcdba65b0c413ed2def06d6e8c4ab23f40eea20b
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-06 01:52:26 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,32 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 from dotenv import load_dotenv
 
-# Initialize extensions
+# Load environment variables
+load_dotenv()
+
 db = SQLAlchemy()
 migrate = Migrate()
-
-# Load environment variables from .env file
-load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
-    # Configurations
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Load DATABASE_URL from environment variables
+    # Load configs from environment variables
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize database and migrations
+    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Register blueprints
+    from app.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
+
 
 
 
