13f056f22f60e907faee962b8efa0f7031a34b13
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-07 22:08:07 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Odds(db.Model):
     __tablename__ = 'odds'
-    __table_args__ = {'extend_existing': True}  # Add this line
+    __table_args__ = {'extend_existing': True}  # Add this line to allow altering the table schema
 
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.String, nullable=False)
@@ -16,6 +16,12 @@ class Odds(db.Model):
     point = db.Column(db.Float, nullable=True)
     timestamp = db.Column(db.String, nullable=False)
     odds_type = db.Column(db.String, nullable=False)
+
+    # New fields
+    completed = db.Column(db.Boolean, nullable=False, default=False)  # Indicates if the event is completed
+    home_score = db.Column(db.Integer, nullable=True)  # Stores the home team's score
+    away_score = db.Column(db.Integer, nullable=True)  # Stores the away team's score
+
     
 
 
