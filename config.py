e5513ae7643f8ab6fb828bed3e702e3cb94a7177
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-12 10:34:26 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from a .env file
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ODDS_API_KEY = os.getenv('ODDS_API_KEY')  # Fetch API key from .env
+    ODDS_API_KEY = os.getenv("ODDS_API_KEY")
