3506ffe67852175b38839682c6b14fc445d3fb22
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-12 12:12:02 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
+# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
 from config import Config
-from .fetch_data import fetch_odds  # Import the fetch_odds function from fetch_data.py
-from .routes import main  # Import the main blueprint
+from .fetch_data import fetch_odds
 
 db = SQLAlchemy()
 
@@ -13,12 +13,9 @@ def create_app():
 
     db.init_app(app)
 
-    # Register the main blueprint
-    app.register_blueprint(main)
-
     # Set up APScheduler to run fetch_odds every 10 minutes
     scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(func=fetch_odds, trigger="interval", minutes=10)
+    scheduler.add_job(func=lambda: fetch_odds(db), trigger="interval", minutes=10)
     scheduler.start()
 
     # Ensure the database is created when the app starts
