9c0702ad3efe22d9fe4049020b61c7c46f0bba63
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-12 10:39:08 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
 from app import create_app
-import os
-from dotenv import load_dotenv
-from app.utils import start_scheduler
 
-# Create the app instance
 app = create_app()
 
-# Load environment variables from .env
-load_dotenv()
-
-# Start the scheduler
-start_scheduler()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
