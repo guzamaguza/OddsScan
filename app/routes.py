0aad2387fe1a0f1bd90b7e83f5b4107ee8e05bc7
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-12 15:48:43 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,13 @@ main = Blueprint("main", __name__)
 
 @main.route('/')
 def home():
-    # Get current time in UTC
+    # Get current time
     now = datetime.utcnow()
 
-    # Get past, ongoing, and future events
-    past_events = OddsEvent.query.filter(OddsEvent.commence_time < now).all()
-    
-    # Correct the condition for ongoing events (commence_time between now and some duration after)
-    ongoing_events = OddsEvent.query.filter(OddsEvent.commence_time <= now, OddsEvent.commence_time > now).all()
-
-    future_events = OddsEvent.query.filter(OddsEvent.commence_time > now).all()
+    # Get past, ongoing, and future events (ensure unique events with distinct)
+    past_events = OddsEvent.query.filter(OddsEvent.commence_time < now).distinct().all()
+    ongoing_events = OddsEvent.query.filter(OddsEvent.commence_time <= now, OddsEvent.commence_time >= now).distinct().all()
+    future_events = OddsEvent.query.filter(OddsEvent.commence_time > now).distinct().all()
 
     return render_template('home.html', past_events=past_events, ongoing_events=ongoing_events, future_events=future_events)
 
