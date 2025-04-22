964b99280755a2a8ff0d41f9aea8eb95b829d5fb
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2025-04-21 21:22:33 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,17 @@
 # app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from apscheduler.schedulers.background import BackgroundScheduler
 from config import Config
+from .fetch_data import fetch_all_data
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)
-    
-    # Store db instance in app context
-    app.db = db
 
     # Import and register blueprints
     from app.routes import main
@@ -26,7 +21,6 @@ def create_app():
     with app.app_context():
         db.create_all()
         print("[INFO] Database tables created")
-        from app.fetch_data import fetch_all_data
         fetch_all_data(db)  # Fetch both odds and scores on startup
 
     # Set up APScheduler to run fetch_all_data every 10 minutes
